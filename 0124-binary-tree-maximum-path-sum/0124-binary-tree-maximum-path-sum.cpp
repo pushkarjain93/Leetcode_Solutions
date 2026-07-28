62ec1790b0168c9e7448cdbe542ab6c4f9d83702
pushkarjain93 <23je0763@iitism.ac.in>
2026-07-28 21:17:00 +0530

    Time: 0 ms (100%), Space: 28 MB (45.99%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -13,10 +13,12 @@ class Solution {
 public:
 int check (TreeNode* root,int &ans){
     if(root == NULL)return 0;
-    int x = check(root->left,ans);
-    int y = check(root->right,ans);
+    int x = max(0,check(root->left,ans));
+    int y = max(0,check(root->right,ans));
+    // if on leftt i am getting a neg sum i will take it as 0
+    // if on right i am getting a neg sum i will take it as 0
     ans=max(ans,x+y+root->val);
-    return max(0,root->val + max(x,y));
+    return root->val + max(x,y);
 }
     int maxPathSum(TreeNode* root) {
         int ans = INT_MIN;
