11a36595fbc49c4ab5eaeca920b70a3d83fcc9dc
pushkarjain93 <23je0763@iitism.ac.in>
2026-08-05 12:33:19 +0530

    Time: 349 ms (34.14%), Space: 344.6 MB (31.38%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -6,56 +6,32 @@ void dfs(int i,vector<int>&mark,vector<vector<int>>&adj,vector<int>&viss){
         if(viss[num]==0) dfs(num,mark,adj,viss);
     }
 }
-void setdfs(int i,int x,vector<int>&mark,vector<vector<int>>&adj,vector<int>&vis){
-   vis[i]=1;
-   for(auto num: adj[i]){
-    if(vis[num]==0){
-    if(x==0){setdfs(num,x,mark,adj,vis);mark[i]=x;}
-   }
-   else{
-    if(x==0)mark[num]=0;
-    if(mark[num]==0)mark[i]=0;
-     }
-   }
-}
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& a) {
-        if(n==60000 && k==59998){
-            vector<int>ans;
-            for(int i=0;i<=59999;i++){
-                ans.push_back(i);
-            }
-            return ans;
-        }
         vector<vector<int>>adj(n);
         for(auto num:a){
             adj[num[0]].push_back(num[1]);
         }
-        // 1 -> 2 
-        // 0 -> 1
-        // 2 -> 0
-        vector<int>mark(n,0);
-        vector<int>vis(n,0);
-        vector<int>viss(n,0);
-        dfs(k,mark,adj,viss);
-       for(int i=0;i<n;i++){
-          if(vis[i]==1)continue;
-          setdfs(i,mark[i],mark,adj,vis);
-       }
-       for(int i = 0;i<n;i++){
-       for(auto num:adj[i]){
-        if(mark[i]==0)mark[num]=0;
-          if(mark[num]==0)mark[i]=0;
-       }
-       }
-       for(int i = 0;i<n;i++){
-       for(auto num:adj[i]){
-        if(mark[i]==0)mark[num]=0;
-          if(mark[num]==0)mark[i]=0;
-       }
-       }
-       vector<int>ans;
-       for(int i=0;i<n;i++){
+     vector<int>mark(n,0);
+     vector<int>vis(n,0);
+     dfs(k,mark,adj,vis);
+     bool xx =false;
+      for(int i=0;i<n;i++){
+        for(auto num:adj[i]){
+            if(mark[i]^mark[num]){xx=true;break;}
+        }
+        if(xx)break;
+      }
+             vector<int>ans;
+        if(xx){
+           for(int i=0;i<n;i++){
+            ans.push_back(i);
+           }
+        }
+       else{
+        for(int i=0;i<n;i++){
             if(mark[i]==0)ans.push_back(i);
-       }return ans;
+        }
+     }
+        return ans;
     }
 };
