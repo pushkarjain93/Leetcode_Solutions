b9e47d516e778b99c0ffd5daa10e6353d3b94d82
pushkarjain93 <23je0763@iitism.ac.in>
2026-08-07 01:04:21 +0530

    Time: 0 ms (100%), Space: 18.1 MB (42.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,13 +6,15 @@ public:
             f[num]++;
         }
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-       for(auto num:f){
-        pq.push({num.second,num.first});
-        if(pq.size()>k)pq.pop();
-       }
-       vector<int>ans;
+        for(auto num:f){
+            pq.push({num.second,num.first});
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        vector<int>ans;
         while(pq.size()>0){
-               ans.push_back(pq.top().second);pq.pop();
+            ans.push_back(pq.top().second);pq.pop();
         }return ans;
     }
 };
