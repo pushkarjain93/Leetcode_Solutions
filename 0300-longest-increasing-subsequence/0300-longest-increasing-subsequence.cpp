ecac8e09faa2df611f9fd957e4fd6c3b0dd4432d
pushkarjain93 <23je0763@iitism.ac.in>
2026-08-18 12:46:59 +0530

    Time: 580 ms (11.36%), Space: 296.4 MB (27.66%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int rec(int i,int prev,vector<int>&a,vector<vector<int>>&dp){
+if(i>=a.size()){
+   return 0;
+}
+if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+   int x =0;
+  if(prev==-1 || a[i]>a[prev]){
+   x = 1+rec(i+1,i,a,dp);
+  }
+  int y = rec(i+1,prev,a,dp);
+  return dp[i][prev+1]=max(x,y);
+}
+    int lengthOfLIS(vector<int>& a) {
+        
+        vector<vector<int>>dp(a.size(),vector<int>(a.size()+1,-1));
+        return rec(0,-1,a,dp);
+    }
+};
