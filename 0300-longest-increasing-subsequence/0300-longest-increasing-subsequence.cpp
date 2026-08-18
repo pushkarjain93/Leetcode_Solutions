3dcd202c845665fb616712d94094c93f8c353285
pushkarjain93 <23je0763@iitism.ac.in>
2026-08-18 12:55:10 +0530

    Time: 680 ms (5.06%), Space: 295.2 MB (32.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,16 +4,20 @@ int rec(int i,int prev,vector<int>&a,vector<vector<int>>&dp){
 if(i>=a.size()){
    return 0;
 }
-if(dp[i][prev+1]!=-1)return dp[i][prev+1];
    int x =0;
-  if(prev==-1 || a[i]>a[prev]){
+if(prev==-1){
+    x = 1+rec(i+1,i,a,dp);
+}
+else{
+if(dp[i][prev]!=-1)return dp[i][prev];
+  if(a[i]>a[prev]){
    x = 1+rec(i+1,i,a,dp);
-  }
+  }}
   int y = rec(i+1,prev,a,dp);
-  return dp[i][prev+1]=max(x,y);
+  if(prev ==-1)return max(x,y);
+  return dp[i][prev]=max(x,y);
 }
     int lengthOfLIS(vector<int>& a) {
-
         vector<vector<int>>dp(a.size(),vector<int>(a.size(),-1));
         return rec(0,-1,a,dp);
     }
