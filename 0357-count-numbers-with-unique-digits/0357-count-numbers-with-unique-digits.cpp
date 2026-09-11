32585f55c8daef353c13996dca28666071a00872
pushkarjain93 <23je0763@iitism.ac.in>
2026-09-11 17:43:10 +0530

    Time: 0 ms (100%), Space: 7.8 MB (72.57%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        vector <int> answer{1, 10, 91, 739, 5275, 32491, 168571, 712891, 2345851}; return answer[n];
+    }
+};
